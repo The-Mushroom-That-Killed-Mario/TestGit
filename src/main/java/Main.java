dd6fa9ccbda56f36558364d4317846947018d02f
The-Mushroom-That-Killed-Mario <andrejdedula@gmail.com>
2023-01-31 21:52:44 +0300

    изменение в ветке 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Test");
         System.out.println("Test");
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
     }
 }
